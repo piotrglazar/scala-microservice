97a134190dc92086ba65c89fda4c26dfb535cfb7
Piotr Glazar <piotr.glazar@gmail.com>
2015-09-16 20:12:49 +0200

    End to end test

--- src/test/scala/com/piotrglazar/scala/MyMicroserviceTest.scala
+++ src/test/scala/com/piotrglazar/scala/MyMicroserviceTest.scala
@@ -1,8 +1,10 @@
 package com.piotrglazar.scala
 
 import akka.event.{LoggingAdapter, NoLogging}
+import akka.http.scaladsl.model.{HttpResponse, HttpRequest}
 import akka.http.scaladsl.testkit.ScalatestRouteTest
-import com.piotrglazar.scala.api.{AddResponse, AddRequest, RandomResponse}
+import akka.stream.scaladsl.Flow
+import com.piotrglazar.scala.api.{CurrenciesResponse, CurrenciesRequest, ExchangeRateApiResponse, AddResponse, AddRequest, RandomResponse}
 import com.piotrglazar.scala.app.{ExchangeRatesService, Service}
 import com.typesafe.config.Config
 import akka.http.scaladsl.model.StatusCodes.OK
@@ -16,6 +18,11 @@ class MyMicroserviceTest extends FlatSpec with Matchers with ScalatestRouteTest 
   override def testConfigSource = "akka.loglevel = WARNING"
   override def config: Config = testConfig
 
+  override lazy val openExchangeRateConnectionFlow = Flow[HttpRequest].map { request =>
+    HttpResponse(status = OK, entity = marshal(ExchangeRateApiResponse("test", "test", 0, "USD", Map("PLN" -> BigDecimal("3.0")))))
+  }
+
+  override val exchangeRatesService: ExchangeRatesService = new ExchangeRatesService
   override val random: TestRandomNumberGenerator = new TestRandomNumberGenerator
   override var logger: LoggingAdapter = NoLogging
 
@@ -62,5 +69,32 @@ class MyMicroserviceTest extends FlatSpec with Matchers with ScalatestRouteTest 
       responseAs[AddResponse].calculationResult shouldBe 8
     }
   }
-  override val exchangeRatesService: ExchangeRatesService = new ExchangeRatesService
+
+  "Service" should "fetch info from ExchangeRates" in {
+    // given
+    val request = CurrenciesRequest(Set("PLN"))
+
+    // when
+    val result = Post("/finance", request) ~> routes
+
+    // then
+    result ~> check {
+      status shouldBe OK
+      responseAs[CurrenciesResponse].rates shouldBe Map("PLN" -> BigDecimal("3.0"))
+    }
+  }
+
+  "Service" should "not break when there is no data for currency" in {
+    // given
+    val request = CurrenciesRequest(Set("PLN", "XYZ"))
+
+    // when
+    val result = Post("/finance", request) ~> routes
+
+    // then
+    result ~> check {
+      status shouldBe OK
+      responseAs[CurrenciesResponse].rates shouldBe Map("PLN" -> BigDecimal("3.0"))
+    }
+  }
 }
